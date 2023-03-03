1f6d4a462aa5423e8a595b1143de74cc9b8d5a88
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-03-03 15:20:03 +0300

    Works with while, do while, for and foreach

---
+++ chapter03/IterationStatements/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using static System.Console;
+
+// While 
+int x = 0;
+while(x < 10)
+{
+    WriteLine(++x);
+}
+
+// Do while
+string? password;
+int errorCount = 1;
+
+do
+{
+    if(errorCount > 10)
+    {
+        Console.WriteLine(errorCount);
+        WriteLine("Your maximum logins allowance is full.");
+        return;
+    }
+
+    Write("Enter the password: ");
+    password = ReadLine();
+    errorCount++;
+} while(password != "abcd");
+
+WriteLine("Correct");
+
+// For
+for(int i = 0; i < 10; i++)
+{
+    WriteLine(i);
+}
+
+// Foreach
+string[] names = {"Yusuf", "Ali", "Veli"};
+
+foreach(string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters");
+}
+
+// How foreach works internally | read-only
+IEnumerator e = names.GetEnumerator();
+
+while(e.MoveNext())
+{
+    string name = (string)e.Current;
+    WriteLine($"{name} has {name.Length} characters");
+}
---
+++ chapter03/IterationStatements/README.md
@@ -0,0 +1,2 @@
+## Neler Yapıldı 
+- for, while, do while ve foreach ifadeleri ile işlemler yaptık.
