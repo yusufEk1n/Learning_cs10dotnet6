58712a40bb5a5b5f27767fd6da14f786d8551fb2
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-03-06 00:39:39 +0300

    Added Exercise04

---
+++ chapter03/Exercise04/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+try
+{
+    Write("Enter a number between 0 and 255: ");
+    byte firstNumber = byte.Parse(ReadLine());
+    
+    Write("Enter another number between 0 and 255: ");
+    byte secondNumber = byte.Parse(ReadLine());
+
+    int answer = firstNumber / secondNumber;
+
+    WriteLine($"{firstNumber} divide by {secondNumber} is {answer}");
+}
+catch(Exception ex)
+{
+    WriteLine($"{ex.GetType()}: {ex.Message}");
+}
