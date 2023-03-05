3fd7969b5efbb72d427e5f840e9e7567645bacd2
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-03-05 23:52:55 +0300

    Added exercise02

---
+++ chapter03/Exercise02/Program.cs
@@ -0,0 +1,16 @@
+﻿int max = 500;
+
+try
+{
+    checked
+    {
+        for(byte i = 0; i < max; i++)
+        {
+           Console.WriteLine(i);
+        }    
+    }
+}
+catch(OverflowException ex)
+{
+    Console.WriteLine($"'{ex.Message}' exception was occured.");
+}
---
+++ chapter03/Exercise02/README.md
@@ -0,0 +1,8 @@
+```C#
+    int max = 500;
+    for(byte i = 0; i < max; i++)
+    {
+        Console.WriteLine(i);
+    }    
+```
+Yukarıdaki kodu çalıştırın ve sorun hakkında kullanıcıyı 'uyaran' bir kod ekleyin.
