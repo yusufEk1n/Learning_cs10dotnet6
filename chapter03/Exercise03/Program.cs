f49e78aee5172ad74555e2abfa746a86d95d3a9e
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-03-06 00:27:44 +0300

    Added Exercise03

---
+++ chapter03/Exercise03/Program.cs
@@ -0,0 +1,31 @@
+﻿for(int i = 1; i <= 100; i++)
+{
+    if(i % 3 == 0 && i % 5 == 0)
+    {
+        Console.Write("FizzBuzz");
+    }
+    else if(i % 5 == 0)
+    {
+        Console.Write("Buzz");
+    }
+    else if(i % 3 == 0)
+    {
+        Console.Write("Fizz");
+    }
+    else
+    {
+        Console.Write($"{i}");
+    }
+
+    // 100'den önceki tüm sayılardan sonra ',' koy.
+    if(i < 100)
+    {
+        Console.Write(", ");
+    }
+
+    // Her 10 sayıda bir satır başı yap.
+    if(i % 10 == 0)
+    {
+        Console.WriteLine();
+    }
+}
